3e5fc382439dde07eeb74210d74ddd8870b7ce12
sudh4rsan <31951326+sudh4rsan@users.noreply.github.com>
2017-09-19 14:34:02 +0530

    Update Anna.java

--- Anna.java
+++ Anna.java
@@ -1,25 +1,24 @@
-import java.util.Scanner;
-public class Bills
-{
-public static void main(String args[])
-{
-Scanner S=new Scanner(System.in);
-int n=S.nextInt();
-int k=S.nextInt();
-int a[]=new int[n];
-for(int i=0;i<n;i++)
-a[i]=S.nextInt();
-int sum=0;
-int chr=S.nextInt();
-for(int i=0;i<n;i++)
-{
-if(i!=k)
-sum=sum+a[i];
-}
-System.out.println(sum);
-if((sum/2-chr)==0)
-System.out.println("Bon Appetit");
-else
-System.out.println(chr-sum/2);
+package kk1;
+
+import java.util.*;
+public class guvipro66 {
+public static void main(String[]args){
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	int k=in.nextInt();
+	int a[]=new int[n];
+	int sum=0;
+	for(int i=0;i<n;i++){
+		a[i]=in.nextInt();
+		}
+	int b=in.nextInt();
+	for(int i=0;i<n;i++){
+		sum=sum+a[i];
+	}
+	if((sum/2)==b){
+		System.out.println(a[k]/2);
+	}else{
+		System.out.println("Bon Appetit");
+	}
 }
 }
